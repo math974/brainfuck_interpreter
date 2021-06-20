8bfccfa9006bf91c934aef33603bd35cc2077834
math974 <mathias.ballot@epitech.com>
2021-06-21 01:36:27 +0400

    implementation of a loop

--- my_brainfuck.py
+++ my_brainfuck.py
@@ -11,6 +11,71 @@ Project : brainfuck interpreter
 Licence : MIT
 """
 
+class Node:
+    """
+    Linked List in python
+    """
+    def __init__(self, value):
+        """
+        construct list
+        """
+        self.value = value
+        self.next = None
+
+class Stack:
+    """
+    Create object Stack
+    """
+    def __init__(self):
+        """
+        init the stack
+        """
+        self.head = Node("head")
+        self.size = 0
+
+    def __str__(self):
+        """
+        String represente the Stack when we print
+        """
+        current = self.head.next
+        recup_value = ""
+        while current:
+            recup_value += str(current.value) + '->'
+            current = current.next
+        return recup_value[:-2]
+
+    def getSize(self):
+        """
+        Get the current size of the stack
+        """
+        return self.size
+
+    def isEmpty(self):
+        """
+        Check if the stack is empty
+        """
+        return self.size == 0
+
+    def push(self, value):
+        """
+        Push the value into the stack
+        """
+        node = Node(value)
+        node.next = self.head.next
+        self.head.next = node
+        self.size += 1
+
+    def pop(self):
+        """
+        Remove the value from the stack and return
+        """
+        if self.isEmpty():
+            raise Exception("Popping from an empty stack")
+        remove = self.head.next
+        self.head.next = self.head.next.next
+        self.size -= 1
+        return remove.value
+
 class My_Brainfuck:
     """
     create object brainfuck
@@ -19,33 +84,84 @@ class My_Brainfuck:
         """
         construct the the object
         """
-        self.i_code = 0
+        self.pc = 0
         self.ptr = 0
+        self.stack_loop = Stack()
+        self.code_progr = []
         self.mem = [0] * 90000
         self.actions = {
-            '<' : (None, self.opp_ptr(-1)),
-            '>' : (None, self.opp_ptr(1)),
-            '+' : (None, self.incr_val(1)),
-            '-' : (None, self.incr_val(-1))
+            '<' : (self._opp_ptr,(-1)),
+            '>' : (self._opp_ptr,(1)),
+            '+' : (self._incr_val,(1)),
+            '-' : (self._incr_val,(-1)),
+            '.' : (self._put_char,(0)),
+            ',' : (self._get_char,(0)),
+            '[' : (self._enter_loop,(0)),
+            ']' : (self._exit_loop,(0))
         }
 
-    def opp_ptr(self, i):
+    def _enter_loop(self, nothing):
+        """
+        enter in the loop
+        """
+        self.stack_loop.push(self.pc)
+
+    def _jmp(self):
+        """
+        jmp to this instruction
+        """
+        label = self.stack_loop.pop()
+        self.pc = label - 1 # intruction avant comme sa il fait + 1 pour ceux retourver à l'instruction '['
+
+    def _exit_loop(self, nothing):
+        """
+        exit in the loop
+        """
+        if self.mem[self.ptr] == 0:
+            self.stack_loop.pop()
+        else:
+            self._jmp()
+
+    def parse_string(self, string):
+        """
+        parse the string programme
+        """
+        for i in range(len(string)):
+            self.code_progr.append(string[i])
+
+    def _opp_ptr(self, i):
         """
         pointer operation
         """
         self.ptr += i
 
-    def incr_val(self, i):
+    def _incr_val(self, i):
         """
         increment the value where is the pointer
         """
         self.mem[self.ptr] += i
 
+    def _put_char(self, nothing):
+        """
+        print the ASCII character
+        """
+        print(chr(self.mem[self.ptr]), sep='', end='')
+
+    def _get_char(self, nothing):
+        """
+        entry of a byte in the array where the pointer is positioned (ASCII value).
+        """
+        self.mem[self.ptr] = ord(sys.stdin.read(1))
+
     def execute_progr(self):
         """
         run program code
         """
-
+        while self.pc < len(self.code_progr):
+            instruction = self.code_progr[self.pc]
+            action, argv = self.actions[instruction] # passer un dictionnaire en arguments de functions
+            action(argv)
+            self.pc += 1
 
 def help_programme():
     """
@@ -109,5 +225,6 @@ if __name__ == "__main__":
         buffer = delate_comment(buffer)
         check_invalid_character(buffer)
         buffer = re.sub(r"[\s]+", "", buffer) # remove white characters
-        print(buffer)
-
+        bf = My_Brainfuck()
+        bf.parse_string(buffer)
+        bf.execute_progr()
